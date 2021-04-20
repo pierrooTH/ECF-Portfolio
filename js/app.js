35a8c4d6ab5736870249bb24dbc94af47955cc86
pierrooTH <pierre.thouvenot1711@gmail.com>
2021-04-20 21:42:00 +0200

    Amélioration du portfolio
    Ajout d'animation JavaScript (ScrollReveal) au scrolling de la page
    
    Modifications dans la section "Projets" : ajout
    d'une flèche descendante pour montrer qu'il y a du contenu à l'intérieur
    des cards

---
+++ js/app.js
@@ -0,0 +1,67 @@
+const scroll = ScrollReveal();
+
+scroll.reveal('.header__maintitle', {
+    duration: 1500,
+    origin: 'top',
+    reset: true,
+    distance: '70px'
+});
+
+scroll.reveal('.header__title', {
+    duration: 1500,
+    origin: 'top',
+    delay: 300,
+    reset: true,
+    distance: '70px'
+});
+scroll.reveal('.section__title--about', {
+    duration: 1500,
+    origin: 'left',
+    delay: 500,
+    reset: true,
+    distance: '70px'
+});
+scroll.reveal('.about__img', {
+    origin: 'top',
+    duration: 1500,
+    delay: 300,
+    reset: true,
+    distance: '100px'
+});
+scroll.reveal('.about__paragraph', {
+    duration: 1500,
+    origin: 'right',
+    delay: 500,
+    reset: true,
+    distance: '90px'
+});
+scroll.reveal('.about__btn', {
+    duration: 1500,
+    delay: 500,
+    reset: true,
+    distance: '70px'
+});
+scroll.reveal('.header__btn', {
+    duration: 1500,
+    delay: 800,
+    reset: true,
+    distance: '70px'
+});
+
+scroll.reveal('.tools', {
+    duration: 2000,
+    origin: 'top',
+    distance: '70px'
+}, 150);
+
+scroll.reveal('.lang', {
+    duration: 2000,
+    distance: '70px'
+}, 150);
+
+scroll.reveal('.project__card', {
+    duration: 2000,
+    origin: 'left',
+    distance: '50px'
+}, 150);
+
